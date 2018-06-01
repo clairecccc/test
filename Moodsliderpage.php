7d4f28493f5e23889a8e4fe19c9972e0c7092e29
Claire <clairecunliffe@gmail.com>
2018-06-01 14:25:19 +0100

    slight ypdate in slider format

--- Moodsliderpage.php
+++ Moodsliderpage.php
@@ -16,32 +16,27 @@
     <div class="container">
          <div class="row">
              
-              <div class="col-4">
+              <div class="col-3">
                     <img src="images/skylogo.jpeg" class="img" alt="Sky Logo">
               </div>
              
-            <div class="col-8">
-                 <div class="m-5 pb-5">
-                    
-                        <h1>Programme recommendations for you</h1>
-                             <a href=Programmeupload.php><h3>Upload Programme information</h3></a>
-                 </div>
+            <div class="col-9">
+                            <h1>Programme recommendations for you</h1>
+                            <a href=Programmeupload.php><h3>Upload Programme information</h3></a>
             </div> 
 
         </div>
     </div>
-    
-    
-    <div class ="jumboton">
-        <div class="slidecontainer">
+
+    <div class="slidecontainer">    
             <div class="row">
-                <div class="col">
+                <div class="col-2">
                     <h4>Agitated</h4>         
                 </div>
-                <div class="col">
+                <div class="col-8">
                 <input type="range" min="1" max="100" value="50" class="slider" id="zen" onchange="filter();">
                 </div>
-                <div class="col">
+                <div class="col-2">
                     <h4>Calm</h4>
                 </div>
             </div>
@@ -49,13 +44,13 @@
     
     <div class="slidecontainer">
             <div class="row">
-                <div class="col">
+                <div class="col-2">
                     <h4>Sad</h4>         
                 </div>
-                <div class="col">
+                <div class="col-8">
                 <input type="range" min="1" max="100" value="50" class="slider" id="happy" onchange="filter();" >
                 </div>
-                <div class="col">
+                <div class="col-2">
                     <h4>Happy</h4>
                 </div>
             </div>
@@ -63,13 +58,13 @@
     
     <div class="slidecontainer">
             <div class="row">
-                <div class="col">
+                <div class="col-2">
                     <h4>Tired</h4>         
                 </div>
-                <div class="col">
+                <div class="col-8">
                 <input type="range" min="1" max="100" value="50" class="slider" id="awakeness" onchange="filter();">
                 </div>
-                <div class="col">
+                <div class="col-2">
                     <h4>Wide Awake</h4>
                 </div>
             </div>
@@ -77,17 +72,18 @@
         
     <div class="slidecontainer">
             <div class="row">
-                <div class="col">
+                <div class="col-2">
                     <h4>Scared</h4>         
                 </div>
-                <div class="col">
+                <div class="col-8">
                 <input type="range" min="1" max="100" value="50" class="slider" id="fear"onchange="filter();">
                 </div>
-                <div class="col">
+                <div class="col-2">
                     <h4>Fearless</h4>
                 </div>
             </div>
-   </div>     
+   </div>
+        
 </div>        
            
     <div id="result">
@@ -155,21 +151,27 @@
         function filter() {
             
             var zenvalue  = document.getElementById("zen").value;
+            var happyvalue = document.getElementById("happy").value;
+            var awakenessvalue = document.getElementById("awakeness").value;
+            var fearvalue = document.getElementById("fear").value;
+            
             var xhttp;
-            console.log(zenvalue)
+            //console.log(zenvalue)
             xhttp = new XMLHttpRequest(); //object instantiation
             xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                   document.getElementById("result").innerHTML = this.responseText
-                }
+                if (this.readyState === 4 && this.status === 200) {
+                   document.getElementById("result").innerHTML = this.responseText;
+                   document.getElementById("result").innerHTML = this.responseText;
+                   document.getElementById("result").innerHTML = this.responseText;
+                   document.getElementById("result").innerHTML = this.responseText;
+                };
             };
-            xhttp.open("GET", "Readprogrammes.php?zen="+zenvalue, true);
+            xhttp.open("GET", "Readprogrammes.php?zen="+zenvalue+"&happy="+happyvalue"&awakeness="+awakenessvalue"&fear="+fearvalue, true);
             xhttp.send();   
         }
     </script> 
      
-   
-    
+
 </body>
 </html>
 
--- Readprogrammes.php
+++ Readprogrammes.php
@@ -4,12 +4,28 @@ $xmlNodes=simplexml_load_file ("Uploads/Programmes.xml");
 $format="£%0.2f\n";
 
 $zenresult =$_GET['zen'];
-$mood=$zenresult;
+$happyresult=$_GET['happy'];
+$awakenessresult=$_GET['awakeness'];
+$fearresult=$_GET['fear'];
 
-if ($zenresult>50) {$mood="Calm";}
-else {$mood="agitated";}
-echo $mood;
+if ($zenresult>50) {$zenresult="Calm";}
+else {$zenresult="Agitated";
+}
 
+if ($happyresult>50) {
+$happyresult="Happy";}
+else {$happyresult="Sad";
+}
+
+if ($awakenessresult>50) {
+$awakenessresult="Wide Awake";}
+else {$awakenessresult="Tired";
+}
+
+if ($fearresult>50) {
+$fearresult="Scared";}
+else {$fearresult="Fearless";
+}
 
 
       
@@ -17,10 +33,7 @@ echo $mood;
         <div class="container">
             <div class="row">
                 <?php foreach($xmlNodes as $programme) {?>
-                
-                <?php if ($mood==$programme->mood) { ?>
-                
-                
+                <?php if ($zenresult || $happyresult || $awaknessresult || $fearresult==$programme->mood) { ?>
                     <div class="col">
                         <div class='card'> 
                             <img class="card-img-top" img src=" <?php echo $programme->imagepath; ?> ">
